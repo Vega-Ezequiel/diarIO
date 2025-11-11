8efea8a0e7f9e38fd37573094b172e51ef7368ab
Ezequiel Vega <vegaezequiel51@gmail.com>
2025-11-11 19:33:48 +0000

    Se agrega Shadcn UI y Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
+	integrations: [react(), tailwind()],
 	adapter: node({
 		mode: 'standalone',
 	}),
